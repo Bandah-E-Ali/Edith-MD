8853a10d93bd2c38a55b79a563a99bb6c542bb96
Dhaniqtz <Chirandanitha652@gmail.com>
2026-07-03 18:30:02 +0530

    Update SESSION_ID in settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SESSION_ID: "", // Add Your Session_ID Here 
+  SESSION_ID: "6bZ2nbwB#wO6UCBsUYY_B-wvTSzRoqygNBibsglFh6uTAhWa8xjc", // Add Your Session_ID Here 
   CDN: "https://bandaheali-cdn.koyeb.app"
 };
